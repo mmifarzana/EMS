ab095af65c573c5a66454165c0bc6288dc8dac88
mmifarzana <mmifarzana@gmail.com>
2021-11-27 23:42:30 +0600

    all employee added

--- Traning/views.py
+++ Traning/views.py
@@ -1,9 +1,16 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
 def view_traning(request): 
+   Tranin = traning.objects.all()
+   context ={
+    "Traning": traning,
+   }
+   return render(request, 'view_traning.html', context)
+   
    return render(request, 'view_traning.html') 
 
 def traning_report(request): 
